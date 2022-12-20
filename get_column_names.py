ca3a896f9cafa75d1ce5ce6f9e1b9be2deea71fc
Ilhom <ilhommamatqulov276@gmail.com>
2022-12-20 20:38:00 +0500

    get_column_names.py

--- get_column_names.py
+++ get_column_names.py
@@ -7,6 +7,10 @@ def get_column_names(data):
     Returns:
         column_names: list of column names
     """
-    return
+    list1=data.split('\n') 
+    return list1[0].split(',')
+f=open('data.csv','r')
+a=f.read()
+print(get_column_names(a))
     
 # Read the csv file
